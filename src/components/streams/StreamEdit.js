ac622b0936efbce16b06cc77a27c11e8de54b3ee
Vlad Vovk <wolfykey@gmail.com>
2020-07-12 20:54:10 +0300

    Fetching a Stream for Edit Stream

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchStream } from 'actions'
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>
+const StreamEdit = ({ match, fetchStream, stream }) => {
+  useEffect(() => {
+    fetchStream(match.params.id)
+  }, [match.params.id, fetchStream])
+
+  if (!stream) {
+    return <div>Loading...</div>
+  }
+
+  return <div>{stream.title}</div>
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+const mapDispatchToProps = {
+  fetchStream
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit)
